da39a89a81d63c458deb85790afb69a0f00a67f9
Anjiswork <visionanjani11@gmail.com>
2022-12-08 09:49:22 +0530

    second-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"calculator",component:CalculatorComponent},
+  {path:"",component:CalculatorComponent},
+ 
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
